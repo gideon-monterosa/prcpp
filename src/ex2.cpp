72797a31bb285676e60a7b6667d85a642a3733ba
gideon <gideon.monterosa@gmail.com>
2024-10-12 17:30:15 +0200

    add ex2

---
+++ src/ex2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "ex1.h"
+
+void sub1() {
+  std::cout << "Subtask 2.1" << std::endl;
+}
+
+void runEx2() {
+  std::cout << "Exercice 2:" << std::endl;
+  sub1();
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
-#include <iostream>
-
 #include "ex1.h"
+#include "ex2.h"
 
 int main() {
   runEx1();
+  runEx2();
 }
